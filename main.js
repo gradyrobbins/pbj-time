de18c690c1c388f9d5ba0f7b5b732ce8a0b79d38
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-22 10:26:22 -0600

    outtatime

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("hello world main.js"); 
+console.log("hello world main.js");
+
+let container = document.getElementById("display-container")
+let pbjbutton = document.createElement("button");
+// pbjbutton.textContent("pbj time");
+pbjbutton.addEventListener("click", console.log("Clicked pbj"));
+
+container.appendChild(pbjbutton);
