20e8d32466910a6d8853e5479892348dbf2f3949
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-22 10:45:31 -0600

    got it

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 console.log("hello world main.js");
 
 let container = document.getElementById("buttongoeshere")
-// let pbjbutton = document.createElement("button");
-// pbjbutton.textContent("button");
-// pbjbutton.addEventListener("click", () => {console.log("Clicked pbj")});
+let display = document.getElementById("display")
 
 
 
@@ -11,7 +9,54 @@ let submitButton = document.createElement("button")
     submitButton.setAttribute("id", "saveInterestsButton")
     submitButton.textContent = "What time is it?"
     // 2. Attach event listener to button in form
-    submitButton.addEventListener("click", () => {console.log("clicked")})
+    submitButton.addEventListener("click", () => {
+        dateTimeBuilder()
+    })
 
 
 container.appendChild(submitButton);
+
+
+const dateTimeBuilder = () => {
+    const today = new Date();
+  const day = today.getDay();
+  const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log(`Today is : ${daylist[day]}.`);
+  let hour = today.getHours();
+  const minute = today.getMinutes();
+  const second = today.getSeconds();
+  let prepand = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ')
+  {
+  if (minute===0 && second===0)
+  {
+  hour=12;
+  prepand=' Noon';
+  }
+  else
+  {
+  hour=12;
+  prepand=' PM';
+  }
+  }
+  if (hour===0 && prepand===' AM ')
+  {
+  if (minute===0 && second===0)
+  {
+  hour=12;
+  prepand=' Midnight';
+  }
+  else
+  {
+  hour=12;
+  prepand=' AM';
+  }
+  }
+
+  
+display.innerHTML = (`Today is : ${daylist[day]}. <br/> Current Time : ${hour}${prepand} : ${minute} : ${second}`);
+
+
+
+}
