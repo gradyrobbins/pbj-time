7e49a98a8d3d2cf5512db09e4ca32ce211ca4fa0
Grady Robbins <gradyrobbins13@gmail.com>
2019-01-22 10:34:07 -0600

    button works; console.logs hello

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 console.log("hello world main.js");
 
-let container = document.getElementById("display-container")
-let pbjbutton = document.createElement("button");
-// pbjbutton.textContent("pbj time");
-pbjbutton.addEventListener("click", console.log("Clicked pbj"));
+let container = document.getElementById("buttongoeshere")
+// let pbjbutton = document.createElement("button");
+// pbjbutton.textContent("button");
+// pbjbutton.addEventListener("click", () => {console.log("Clicked pbj")});
 
-container.appendChild(pbjbutton);
+
+
+let submitButton = document.createElement("button")
+    submitButton.setAttribute("id", "saveInterestsButton")
+    submitButton.textContent = "What time is it?"
+    // 2. Attach event listener to button in form
+    submitButton.addEventListener("click", () => {console.log("clicked")})
+
+
+container.appendChild(submitButton);
